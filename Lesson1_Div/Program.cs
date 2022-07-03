17e2f76496dd06617c37e9e3e900d9ff6f79ade0
SavlaS <savlas93@mail.ru>
2022-07-03 16:05:46 +0300

    Добавили Определение Double

--- Lesson1_Div/Program.cs
+++ Lesson1_Div/Program.cs
@@ -1,3 +1,3 @@
-﻿int numberA = 10;                     //  double numberA = 12   используем double для деления всех чисел
+﻿int numberA = 10;                     //  double numberA = 12   используем double - для вещественных типов данных
 int numberB = 5;                      //  double numberB = 5    
 Console.Writeline(NumberA / NumberB);
