84cfaf77b0a5a4611d9dd6c017df51b58ca83a1c
SavlaS <savlas93@mail.ru>
2022-07-03 16:36:57 +0300

    добавили МАШУ!

--- Lesson1_Condition_If_Else/Program.cs
+++ Lesson1_Condition_If_Else/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Writeline("Введите имя пользователя: ");
+string usernam = Console.Readline();
+
+if(username.Tolower() == "маша")
+{
+    Console.Writeline("УРа, это же МАША");
+}
+else
+{
+    COnsole.Write("Привет, ");
+    Console.Writeline(username);
+}
