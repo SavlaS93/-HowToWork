2c016465a7434e0cce34f510cef73c6f4c7fccf9
SavlaS <savlas93@mail.ru>
2022-07-03 14:48:44 +0300

    add HelloConsole

---
+++ Lection1_HelloUser/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Writeline("Введите ваше имя ");
+string username = Console.Readline();
+Console.Writeline("Привет, ");
+Console.Writeline(username);
+//Console.Writeline(); выводит в сообщение с новой строки . Например: Ввод имени
+//Алексей 
+//Привет ,
+//Алексей
+//Console.Write(); выводит сообщение в одной строке . Например: Ввод имени
+//Алексей
+//Привет , Алексей
+//ReadLine(); Считывает строку из терминала
