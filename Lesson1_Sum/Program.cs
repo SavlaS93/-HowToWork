d4000c2c6788850c1c9e0cccbcfd1a5d08ed58d2
SavlaS <savlas93@mail.ru>
2022-07-03 16:20:37 +0300

    Правочка

--- Lesson1_Sum/Program.cs
+++ Lesson1_Sum/Program.cs
@@ -1,5 +1,13 @@
 ﻿int numberA = 3;
 int numberB = 5; 
 int result = numberA + numberB;
-Console.Writeline(result); //можно сделать так или >> Console.Writeline(Number + NumberB)
+Console.Writeline(result); //можно сделать так или >> Console.Writeline(Number + NumberB);
 
+
+
+//int NumberA = new Random().Next(1, 10); // 1,2,3,4 ... 10
+//Console.Writeline(numberA);             //показывает какое число сгенерировалось в NumberA
+//int NumberB = new Random().Next(1, 10); 
+//Console.Writeline(numberB);             //показывает какое число сгенерировалось в NumberB
+//int result(NumberA + NumberB);
+//Console.Writeline(result);
